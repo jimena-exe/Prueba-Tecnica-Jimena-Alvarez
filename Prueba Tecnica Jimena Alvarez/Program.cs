9760baa6a6c7ecf1488f922074672c99b41d3a2f
jimena-exe <69021513+jimena-exe@users.noreply.github.com>
2022-10-01 18:02:46 -0500

    Initial project file

---
+++ Prueba Tecnica Jimena Alvarez/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ejercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Prueba tecnica jimena alvarez");
+            
+
+        }
+
+    }
+}
