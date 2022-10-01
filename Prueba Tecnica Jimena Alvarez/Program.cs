037ae53c8fab731892ddcfb86a61f1cc15c3ff53
jimena alvarez <jimena.alvarezva@amigo.edu.co>
2022-10-01 18:46:30 -0500

    fixing merging file problem

--- Prueba Tecnica Jimena Alvarez/Program.cs
+++ Prueba Tecnica Jimena Alvarez/Program.cs
@@ -6,8 +6,19 @@
         {
 
             Console.WriteLine("Prueba tecnica jimena alvarez");
-            
 
+            Console.WriteLine("Primer ejercicio");
+            string mensaje = "ejemplo primera funcionalidad añadida";
+            ejercicioUno uno = new ejercicioUno(mensaje[0..20]);
+
+            Console.WriteLine("Segundo ejercicio");            
+            ejercicioDos dos = new ejercicioDos(30);
+
+            Console.WriteLine("Tercer ejercicio");
+            int[] arreglo2 = { 2, 5, 7, 4, 23, 45, 66, 90, 10, 76, 32, 11, 8, 30, 20, 78, 53 };
+            ejercicioTres tres = new ejercicioTres(arreglo2);            
+
+            Console.WriteLine("fin de la aplicacion");
         }
 
     }
---
+++ Prueba Tecnica Jimena Alvarez/ejercicio1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ejercicios
+{
+    public class ejercicioUno
+    {
+        public ejercicioUno(string mensaje)
+        {
+            for (int i = 0; i < mensaje.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine(mensaje + "...");
+                }
+
+            }
+
+        }
+    }
+}
+
