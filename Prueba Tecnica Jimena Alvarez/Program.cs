64cba32360db75a0433ddd310af241ad3d9dfaba
jimena alvarez <jimena.alvarezva@amigo.edu.co>
2022-10-01 18:29:39 -0500

    updating second task

--- Prueba Tecnica Jimena Alvarez/Program.cs
+++ Prueba Tecnica Jimena Alvarez/Program.cs
@@ -11,7 +11,8 @@
             string mensaje = "ejemplo primera funcionalidad añadida";
             ejercicioUno uno = new ejercicioUno(mensaje[0..20]);
 
-
+            Console.WriteLine("Segundo ejercicio");            
+            ejercicioDos dos = new ejercicioDos(30);
 
             Console.WriteLine("fin de la aplicacion");
         }
---
+++ Prueba Tecnica Jimena Alvarez/ejercicio2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicios
+{
+    internal class ejercicio2
+    {
+    }
+
+    public class ejercicioDos
+    {
+        public ejercicioDos(int numero)
+        {
+            int x = 0;
+            int y = 1;
+            Console.WriteLine("1");
+            for (int i = 0; i < numero; i++)
+            {
+                int z = x + y;
+                Console.WriteLine(z + " ");
+                x = y;
+                y = z;
+            }
+            Console.WriteLine("sí cumple!");
+            Console.ReadKey();
+
+        }
+    }
+}
