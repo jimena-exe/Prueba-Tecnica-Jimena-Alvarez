6fd6b132bd0b4e669b1dae26887feb631589d854
jimena alvarez <jimena.alvarezva@amigo.edu.co>
2022-10-01 19:08:49 -0500

    updating four pending task

---
+++ Prueba Tecnica Jimena Alvarez/ejercicio4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicios
+{
+    public class Car
+    {
+        public string Branch { get; set; }
+        public int Model { get; set; }
+        public string Color { get; set; }
+
+
+        public Car(string Branch, int Model, string Color)
+        {
+            this.Branch = Branch;
+            this.Model = Model;
+            this.Color = Color;
+
+        }
+
+    }
+}
